c37d1759342411f8d58d57df118c69bccaa3590d
stamati <andreinustamati@gmail.com>
2024-09-11 17:26:09 +0300

    Modal&Form Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs'
+import { ModalProvider } from "@/providers/modal-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ModalProvider />
         {children}
       </body>
     </html>
