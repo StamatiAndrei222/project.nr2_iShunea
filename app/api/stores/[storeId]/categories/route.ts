2bc2c9ab3383e5da86a6be7c57a6c896cf08a92b
stamati <andreinustamati@gmail.com>
2024-09-20 15:33:00 +0300

    Categories Entity

--- app/api/stores/[storeId]/categories/route.ts
+++ app/api/stores/[storeId]/categories/route.ts
@@ -11,18 +11,18 @@ export async function POST(
     //console.log("UserId:", userId);  
     const body = await req.json();
 
-    const { label, imageUrl } = body;
+    const { name, billboardId } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
     }
 
-    if (!label) {
-      return new NextResponse("Label is required", { status: 400 });
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
     }
 
-    if (!imageUrl) {
-        return new NextResponse("Image URL is required", { status: 400 });
+    if (!billboardId) {
+        return new NextResponse("Billboard Id is required", { status: 400 });
       }
 
 if (!params.storeId){
@@ -41,17 +41,17 @@ const storeByUserId = await prismadb.store.findFirst({
   }
   
 
-    const billboard = await prismadb.billboard.create({
+    const category = await prismadb.category.create({
       data: {
-        label,
-        imageUrl,
+        name,
+        billboardId,
         storeId: params.storeId
       }
     });
 
-    return NextResponse.json(billboard);
+    return NextResponse.json(category);
   } catch (error) {
-    console.log('[BILLBOARDS_POST]', error);
+    console.log('[CATEGORIES_POST]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
@@ -66,15 +66,15 @@ if (!params.storeId){
     return new NextResponse("Store Id is required", { status: 400 });
 }
 
-    const billboards = await prismadb.billboard.findMany({
+    const categories = await prismadb.category.findMany({
       where:{
         storeId: params.storeId,
       }
     });
 
-    return NextResponse.json(billboards);
+    return NextResponse.json(categories);
   } catch (error) {
-    console.log('[BILLBOARDS_GET]', error);
+    console.log('[CATEGORIES_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
